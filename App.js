26ff49551b156b79cee3dd504ff1a14a9cc312dd
Hamid313-Coder <hamid.hussainy0@gmail.com>
2022-06-19 10:53:20 +0430

    This commit is about learning how to work with Animated.spring function

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from "react";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePressIn = this.handlePressIn.bind(this);
+    this.handlePressOut = this.handlePressOut.bind(this);
+    this.animatedValue = new Animated.Value(1);
+  }
+
+  handlePressIn() {
+    Animated.spring(this.animatedValue, {
+      toValue: 0.5,
+    }).start();
+  }
+
+  handlePressOut() {
+    Animated.spring(this.animatedValue, {
+      toValue: 1,
+      friction: 4,
+      // tension: 40,
+    }).start();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableWithoutFeedback
+          onPressIn={this.handlePressIn}
+          onPressOut={this.handlePressOut}
+        >
+          <Animated.View
+            style={[styles.box, { transform: [{ scale: this.animatedValue }] }]}
+          >
+            <Text style={{ color: "white" }}>Press Me</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box: {
+    backgroundColor: "dodgerblue",
+    width: 200,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
   },
 });
